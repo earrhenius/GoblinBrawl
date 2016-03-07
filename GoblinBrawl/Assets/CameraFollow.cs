d48be2930afa14f3a1f1349afc219fde0509f9a4
earrhenius <erikarrhenius@gmail.com>
2016-03-08 00:40:34 +0100

    updated Camera to follow player

--- GoblinBrawl/Assets/CameraFollow.cs
+++ GoblinBrawl/Assets/CameraFollow.cs
@@ -4,12 +4,20 @@ using System.Collections;
 public class CameraFollow : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-	
+	[SerializeField]
+	private float smooth;
+
+	[SerializeField]
+	private Transform targetObject;
+
+	private Transform cameraObject;
+	void Awake()
+	{
+		cameraObject = gameObject.transform;
 	}
-	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
+		cameraObject.transform.position = Vector3.Lerp(cameraObject.transform.position,targetObject.position,smooth);
 	}
 }
